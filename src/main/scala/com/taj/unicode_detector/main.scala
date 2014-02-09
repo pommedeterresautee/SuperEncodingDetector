8d2289e2fbd1ede608b1a12ed8747f1892c50498
El Potaeto <contact@2borange.com>
2014-02-09 22:03:48 +0100

    fix a init file access bug

--- src/main/scala/com/taj/unicode_detector/main.scala
+++ src/main/scala/com/taj/unicode_detector/main.scala
@@ -45,21 +45,24 @@ class TheLogger extends Actor {
   }
 }
 
-class FileAnalyzer(logger:ActorRef, nrOfWorkers: Int, lengthFileToAnalyze:Int) extends Actor {
-  val start = System.currentTimeMillis
-  val workerRouter = context.actorOf(Props[BlockAnalyzer].withRouter(RoundRobinRouter(nrOfWorkers)), name = "workerRouter")
-  val lengthBlockToAnalyze = lengthFileToAnalyze / nrOfWorkers
-  var analyzeResult = true
-  var resultReceived = 0
+class FileAnalyzer(logger:ActorRef, nbrOfWorkers: Int, totalLengthToAnalyze:Int) extends Actor {
+  val startTime = System.currentTimeMillis // to compute time elapsed to give a result
+  val router = context.actorOf(Props[BlockAnalyzer].withRouter(RoundRobinRouter(nbrOfWorkers)), name = "workerRouter")
+  val lengthPerWorkerToAnalyze = totalLengthToAnalyze / nbrOfWorkers
+  var resultOfAnalyze = true // init
+  var resultReceived = 0 // init
 
   def receive = {
     case AnalyzeFile(path) =>
-      println("Enter in Master")
-      (0 to nrOfWorkers-1).foreach(workerNbr => workerRouter ! AnalyzeBlock(path, workerNbr * lengthBlockToAnalyze, lengthBlockToAnalyze))
-    case Result(isBlockASCII) => resultReceived += 1
-      analyzeResult &= isBlockASCII
-      if(resultReceived == nrOfWorkers || !analyzeResult){
-        logger ! ToPrint(if (isBlockASCII) "ascii" else "non ascii", System.currentTimeMillis() - start)
+      println("Start the processing...")
+      (0 to nbrOfWorkers-1)
+        .foreach(workerNbr => 
+        router ! AnalyzeBlock(path, workerNbr * lengthPerWorkerToAnalyze, lengthPerWorkerToAnalyze))
+    case Result(isBlockASCII) => 
+      resultReceived += 1
+      resultOfAnalyze &= isBlockASCII
+      if(resultReceived == nbrOfWorkers || !resultOfAnalyze){
+        logger ! ToPrint(if (isBlockASCII) "ascii" else "non ascii", System.currentTimeMillis() - startTime)
         context.stop(self)
       }
   }
@@ -70,21 +73,21 @@ class BlockAnalyzer extends Actor {
   def receive = {
     case AnalyzeBlock(bigDataFilePath, startRead, length) =>
       val ID = startRead / length
-      println(s"Enter in Worker $ID @$startRead - ${startRead + length - 1}")
+      println(s"Start analyze of block $ID [$startRead - ${startRead + length - 1}]")
       sender ! analyzeBlock(bigDataFilePath, startRead, length)
-      println(s"Quit Worker $ID")
+      println(s"Stop analyze of block $ID")
   }
 
-  def analyzeBlock(path: String, startRead: Long, length: Int): Result = {
+  def analyzeBlock(path: String, startRead: Long, lengthOfBlockToAnalyze: Int): Result = {
     val randomAccessFile = new RandomAccessFile(path, "r")
+    randomAccessFile.seek(startRead)
     var isASCII = false
     try {
       isASCII = Iterator
         .continually(randomAccessFile.read())
-        .takeWhile(c => c != -1)
-        .take(length)
+        .takeWhile(c => c != -1) // stop when the end of file is reached
+        .take(lengthOfBlockToAnalyze)
         .forall(Character.UnicodeBlock.of(_) == Character.UnicodeBlock.BASIC_LATIN)
-
     } finally {
       randomAccessFile.close()
     }
@@ -105,7 +108,7 @@ object main extends App {
 
   val system = ActorSystem("AsciiDetector")
 
-  val logger = system.actorOf(Props[TheLogger], name = "listener")
+  val logger = system.actorOf(Props[TheLogger], name = "LoggerWorker")
   val master = system.actorOf(Props(new FileAnalyzer(logger, workerCount, bytesToRead)), name = "FileAnalyzer")
 
   master ! AnalyzeFile(pathToFEC)
